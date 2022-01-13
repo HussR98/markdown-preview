131dfd3f456262f6315bd44ef61739d8956d311e
HussR98 <huss.rizvi78@gmail.com>
2022-01-13 13:10:36 -0500

    update toggle styles

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { ToggleWrapper, SwitchWrapper } from "./toggle.styles";
+import { ToggleWrapper, SwitchWrapper, LabelWrapper1, LabelWrapper2 } from "./toggle.styles";
 
 
 
 const Toggle = ({label1 = 'on', label2 = 'off', setting = true, onClick}) => {
 
 
-    return <ToggleWrapper onClick = {onClick}><SwitchWrapper animate = {setting ? { x: 0 } : { x: '100%' }} /></ToggleWrapper>
+    return <ToggleWrapper onClick = {onClick}>
+    <SwitchWrapper animate = {setting ? { x: 0 } : { x: '100%' }} />
+    <LabelWrapper1 setting = {setting}>{label1}</LabelWrapper1>
+    <LabelWrapper2 setting = {setting}>{label2}</LabelWrapper2>
+    </ToggleWrapper>
 }
 
 export default Toggle;
--- src/components/toggle.styles.js
+++ src/components/toggle.styles.js
@@ -2,16 +2,39 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 export const ToggleWrapper = styled.div`
-    display: flex;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
     align-items: center;
     grid-row: 2/3;
     grid-column: 2/3;
     width: 20vw;
-    background-color: black;
     justify-self: center;
+    border: 3px solid rgb(29 161 242);;
+    border-radius: 40px;
+    padding: 5px 3%;
+    font-weight: bold;
 `
 export const SwitchWrapper = styled(motion.div)`
-    width: 50%;
-    height: 90%;
-    background-color: pink;
+    width: 100%;
+    height: 100%;
+    background-color: rgb(29 161 242);
+    grid-row: 1/2;
+    grid-column: 1/2;
+    border-radius: 40px;
+`
+
+export const LabelWrapper1 = styled.div`
+    grid-row: 1/2;
+    grid-column: 1/2;
+    z-index: 1;
+    user-select: none;
+    color: ${props => props.setting ? 'white' : 'silver'}
+`
+export const LabelWrapper2 = styled.div`
+    grid-row: 1/2;
+    grid-column: 2/3;
+    z-index: 1;
+    user-select: none;
+    color: ${props => !props.setting ? 'white' : 'silver'}
 `
