19dec452fd011fe9ce2a85be7d390cd112852505
HussR98 <huss.rizvi78@gmail.com>
2022-01-13 13:40:19 -0500

    update button styles

--- src/components/button.js
+++ src/components/button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ButtonWrapper } from './button.styles';
 
-const Button = ({label = 'click me'}) => {
-    return <ButtonWrapper>{label}</ButtonWrapper>
+const Button = ({label = 'click me', operation, data}) => {
+    return <ButtonWrapper onClick = {operation(data)}>{label}</ButtonWrapper>
 }
 
 export default Button;
--- src/components/button.styles.js
+++ src/components/button.styles.js
@@ -1,7 +1,24 @@
 import styled from "styled-components";
 
 export const ButtonWrapper = styled.div`
+    display: grid;
+    width: 20%;
+    justify-self: end;
+    border: 3px solid rgb(29 161 242);
+    border-radius: 40px;
+    padding: 5px 3%;
+    font-weight: bold;
+    align-items: center;
+    user-select: none;
+    color: silver;
+
     grid-row: 2/3;
     grid-column: 1/2;
-    justify-self: center;
+
+    transition: all 0.1s;
+    
+    :hover {
+        background-color: rgb(29 161 242);
+        color: white;
+    }
 `
